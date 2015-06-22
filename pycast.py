bc1ae764d111ca632ae0d7145480c7edd0234577
Guled <somnibyte@gmail.com>
2015-06-21 20:14:47 -0500

    Pycast can now download a music file onto your computer, but cannot play from terminal (yet).

--- pycast.py
+++ pycast.py
@@ -26,20 +26,31 @@ def list():
         print("{} : {}".format(index + 1,podcast))
 
 @cli.command()
-@click.option('--podcast', prompt='What podcast do you want to list to?')
+@click.option('--podcast', prompt='What podcast do you want to listen to? [Enter the users SoundCloud username]')
 def play(podcast):
     # Sound Cloud Integration
-    """
-    client = soundcloud.Client(client_id='CLIENT-ID-GOES-HERE')
-    tracks = client.get('/tracks', limit=10)
-    for track in tracks:
-        print track.title
-    app = client.get('/apps/124')
-    print app.permalink_url
-    """
-    # find all sounds of buskers licensed under 'creative commons share alike'
-    client = soundcloud.Client(client_id='CLIENT-ID-GOES-HERE')
-    tracks = client.get('/tracks', q=podcast, license='cc-by-sa')
+        client = soundcloud.Client(client_id='CLIENT-ID-GES-HERE')
+        podcastName = str(podcast)
+
+        try:
+            tracks = client.get('/users/{}/tracks'.format(podcastName),limit=1)
+            for track in tracks:
+                stream_url = client.get(track.stream_url, allow_redirects=False)
+
+            print stream_url.location
+            url = stream_url.location
+            file = urlopen(url)
+            fileManager = open('test.mp3','wb')
+            fileManager.write(file.read())
+            fileManager.close()
+            music = pyglet.resource.media('test.mp3')
+            music.play()
+            pyglet.app.run()
+
+        except Exception, e:
+            print 'Error: %s, Status Code: %d' % (e.message, e.response.status_code)
+
+
 
 
 """
